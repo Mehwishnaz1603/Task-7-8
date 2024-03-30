50e1bbca44b55088b7c130f3525e9e9a40ad1d4b
Mehwishnaz <160154061+Mehwishnaz1603@users.noreply.github.com>
2024-03-30 17:18:36 +0500

    This is task no 7 and 8

---
+++ index.ts
@@ -0,0 +1,20 @@
+//Task # 7// And Task 8//
+
+//Number Eight: Write addition, subtraction, multiplication, and division operations that each 
+//result in the number 8. Be sure to enclose your operations in print statements to see the results.
+//You should create four lines that look like this:
+//console.log(5 + 3) //
+//Your output should simply be four lines with the number 8 appearing once on each line.//
+
+//In Addition Case//
+console.log(5+3);
+
+//In Substract Case//
+console.log(11-3);
+
+//In Multiplication Case//
+console.log(4*2);
+
+//In Division Case//
+console.log(16/2);
+
